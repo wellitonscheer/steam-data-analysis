9d146aa2d1b966e1cc1571dbde28c559b2c352be
caie <wellitonscheer@gmail.com>
2025-08-22 16:14:35 -0300

    feat: split functionalities

--- steam_analizer/analizer.py
+++ steam_analizer/analizer.py
@@ -1,27 +1,42 @@
 import csv
 from plataform_tags import PlataformTags
 
-with open("dataset/steam_games.csv") as csvfile:
-    reader = csv.reader(csvfile, delimiter=",")
-
-    headers = []
-    data = []
+class SteamAnalizer:
+    def __init__(self, csv_file):
+        self.csv_file = csv_file
+        self.data = []
+        self.plataform_tags = PlataformTags()
     
-    plataform_tags = PlataformTags()
+    def process_csv(self):
+        """
+        processa o arquivo csv e carrega os dados em uma lista
+        """
+        with open(self.csv_file) as csvfile:
+            reader = csv.reader(csvfile, delimiter=",")
 
-    for row in reader:
-        if reader.line_num == 1:
-            headers = row
-            continue
+            headers = []
+            for row in reader:
+                if reader.line_num == 1:
+                    headers = row
+                    continue
 
-        record = {header: row[i] for i, header in enumerate(headers)}
-        data.append(record)
+                record = {header: row[i] for i, header in enumerate(headers)}
+                self.data.append(record)
 
-        plataform_tags.process_record(record)
-
-        # if reader.line_num == 10:
-        #     break
+    def process_data(self):
+        """
+        processa todos os registros para todas as funcionalidades do analizador
+        """
+        for record in self.data:
+            self.plataform_tags.process_record(record)
     
-    info = plataform_tags.get_info()
-    
-    print(info)
+    def process_plataform_tags(self, record=None):
+        """
+        processa um registro ou todos os registros
+        """
+        if record:
+            self.plataform_tags.process_record(record)
+            return
+
+        for record in self.data:
+            self.plataform_tags.process_record(record)
