6b749fb376b74fd22eb4a7f51d0be07e9c4e0d6d
caie <wellitonscheer@gmail.com>
2025-08-22 17:11:28 -0300

    test: plataform tags

---
+++ tests/test_plataform_tags.py
@@ -0,0 +1,14 @@
+import os
+import pytest
+from steam_analizer.analizer import SteamAnalizer
+
+class TestPlataformTags:
+    def setup_method(self):
+        csv_file = os.path.join(os.path.dirname(__file__), "dataset", "steam_games.csv")
+        self.analizer = SteamAnalizer(csv_file)
+        self.analizer.process_csv()
+        self.analizer.process_data()
+    
+    def test_plataform_tags(self):
+        print(self.analizer.plataform_tags.get_info())
+        assert True
