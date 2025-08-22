f82557686b015ea83f5b48e6e180a7fc6be588f4
caie <wellitonscheer@gmail.com>
2025-08-22 10:07:12 -0300

    feat: class for each plataform

--- steam_analizer/plataform_tags.py
+++ steam_analizer/plataform_tags.py
@@ -1,32 +1,57 @@
-class PlataformTags:
-    def __init__(self):
-        self.linux_tags = {}
-        self.windows_tags = {}
-        self.mac_tags = {}
+class PlataformTag:
+    def __init__(self, plataform):
+        self.plataform = plataform
+        self.tags = {}
+        self.plataform_ocurrence = 0
+
+    def __str__(self):
+        return f"{self.plataform}: {self.tags}"
+    
+    def add_tag(self, tag):
+        self.tags[tag] = self.tags.get(tag, 0) + 1
     
     def process_record(self, record):
-        if record["Linux"] == "True":
-            for tag in record["Tags"].split(","):
-                self.linux_tags[tag] = self.linux_tags.get(tag, 0) + 1
-
-        if record["Windows"] == "True":
-            for tag in record["Tags"].split(","):
-                self.windows_tags[tag] = self.windows_tags.get(tag, 0) + 1
-
-        if record["Mac"] == "True":
-            for tag in record["Tags"].split(","):
-                self.mac_tags[tag] = self.mac_tags.get(tag, 0) + 1
+        for tag in record["Tags"].split(","):
+            self.add_tag(tag)
+            self.plataform_ocurrence += 1
     
     def sort_tags(self):
-        self.linux_tags = sorted(self.linux_tags.items(), key=lambda x: x[1], reverse=True)
-        self.windows_tags = sorted(self.windows_tags.items(), key=lambda x: x[1], reverse=True)
-        self.mac_tags = sorted(self.mac_tags.items(), key=lambda x: x[1], reverse=True)
+        self.tags = sorted(self.tags.items(), key=lambda x: x[1], reverse=True)
     
     def get_info(self):
         self.sort_tags()
         return {
-            "linux_tags": self.linux_tags[:10],
-            "windows_tags": self.windows_tags[:10],
-            "mac_tags": self.mac_tags[:10]
+            "plataform": self.plataform,
+            "tags": self.tags,
+            "plataform_ocurrence": self.plataform_ocurrence
+        }
+
+class PlataformTags:
+    def __init__(self):
+        self.linux_tags = PlataformTag("Linux")
+        self.windows_tags = PlataformTag("Windows")
+        self.mac_tags = PlataformTag("Mac")
+    
+    def process_record(self, record):
+        if record["Linux"] == "True":
+            self.linux_tags.process_record(record)
+
+        if record["Windows"] == "True":
+            self.windows_tags.process_record(record)
+
+        if record["Mac"] == "True":
+            self.mac_tags.process_record(record)
+    
+    def sort_tags(self):
+        self.linux_tags.sort_tags()
+        self.windows_tags.sort_tags()
+        self.mac_tags.sort_tags()
+    
+    def get_info(self):
+        self.sort_tags()
+        return {
+            "linux_tags": self.linux_tags.get_info(),
+            "windows_tags": self.windows_tags.get_info(),
+            "mac_tags": self.mac_tags.get_info()
         }
         
